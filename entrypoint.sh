7db0af096244ac291b5683f2385120f0d7405541
o2uf7rt4 <o2uf7rt4@pm.me>
2022-07-19 21:04:07 +0300

    changes:  M Dockerfile, M entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
+gunicorn svfoundation.wsgi:application --bind 0.0.0.0:8000
