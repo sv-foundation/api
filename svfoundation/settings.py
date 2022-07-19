3d48f73ecad52824c98abb7aada692afb4de8824
alex <sashayak2203@gmail.com>
2022-07-19 14:03:51 +0300

    added env.example

--- svfoundation/settings.py
+++ svfoundation/settings.py
@@ -95,9 +95,9 @@ WSGI_APPLICATION = 'svfoundation.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_HOST', 'svfoundation_db'),
+        'NAME': os.environ.get('DB_NAME', 'svfoundation_db'),
         'USER': os.environ.get('DB_USR', 'postgres'),
-        'PASSWORD': os.environ['DB_PASSWORD'],
+        'PASSWORD': os.environ['DB_PWD'],
         'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
         'PORT': os.environ.get('DB_PORT', '127.0.0.1')
     }
