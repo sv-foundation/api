9f3dedcac10e66d980842d4853925bfbabc28fbf
alex <sashayak2203@gmail.com>
2022-07-21 16:37:34 +0300

    updated help requests admin, removed  attributes from News.content

--- help/models.py
+++ help/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.datetime_safe import strftime
 from django_extensions.db.models import TimeStampedModel
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -11,7 +12,7 @@ class HelpRequest(TimeStampedModel):
     message = models.TextField(null=False)
 
     def __str__(self):
-        return f'{self.full_name} {self.created}'
+        return f'Help request from: {self.full_name}; time: {strftime(self.created, "%H:%M %d-%m-%Y")}'
 
 
 class Document(TimeStampedModel):
